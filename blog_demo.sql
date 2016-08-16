9ba9adc3ecade512f2c7a48f4a4dbb014a5ff07d
Shashwat Dixit <shashwatdixit124@gmail.com>
2016-08-17 00:15:47 +0530

    changed sql file

--- blog_demo.sql
+++ blog_demo.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.5.0.2
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Aug 15, 2016 at 01:30 PM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.5.37
+-- Host: localhost
+-- Generation Time: Aug 16, 2016 at 08:44 PM
+-- Server version: 10.0.17-MariaDB
+-- PHP Version: 5.6.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -28,10 +28,18 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `users_content` (
   `id` int(11) NOT NULL,
-  `name` int(11) NOT NULL,
-  `heading` varchar(50) NOT NULL,
-  `body` varchar(1000) NOT NULL
+  `title` varchar(100) NOT NULL,
+  `content` text NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `userid` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users_content`
+--
+
+INSERT INTO `users_content` (`id`, `title`, `content`, `time`, `userid`) VALUES
+(8, 'blog from web', 'BKUYKGBN', '2016-08-16 18:43:46', 10);
 
 -- --------------------------------------------------------
 
@@ -51,9 +59,8 @@ CREATE TABLE `users_list` (
 --
 
 INSERT INTO `users_list` (`id`, `name`, `password`, `email`) VALUES
-(4, 'Dhruvraj', '1eba9614763773df08dd49049663c3', 'dhruv@gmail.com'),
-(5, 'Dhruvraj', 'dhruv', 'dhruv@gmail.com'),
-(6, 'Saket', 'saket', 'saket@gmail.com');
+(9, 'Shashwat', 'csKtuqY2hCyJU', 'shashwat@gmail.com'),
+(10, 'dhruv', 'csEGmVrt.QZ.E', 'dhruv@gmail.com');
 
 --
 -- Indexes for dumped tables
@@ -63,7 +70,8 @@ INSERT INTO `users_list` (`id`, `name`, `password`, `email`) VALUES
 -- Indexes for table `users_content`
 --
 ALTER TABLE `users_content`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userid` (`userid`);
 
 --
 -- Indexes for table `users_list`
@@ -79,12 +87,22 @@ ALTER TABLE `users_list`
 -- AUTO_INCREMENT for table `users_content`
 --
 ALTER TABLE `users_content`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `users_list`
 --
 ALTER TABLE `users_list`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `users_content`
+--
+ALTER TABLE `users_content`
+  ADD CONSTRAINT `id-userid` FOREIGN KEY (`userid`) REFERENCES `users_list` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- conn/config.php
+++ conn/config.php
@@ -4,7 +4,7 @@
 
 		$servername = "localhost";
 		$username = "root";
-		$password = "";
+		$password = "shyam";
 		$dbname = "blog_demo";
 		$conn;
 
